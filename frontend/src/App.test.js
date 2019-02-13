20182bb6db9bdfdd2a371db22f853d1c86da2326
Arthur Bailão <arthurbailao@gmail.com>
2019-02-13 19:52:37 -0200

    writing some tests

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,11 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Chat from "./Chat";
-
-import auth from "./auth";
+import App from "./App";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Chat auth={auth} />, div);
+  ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
--- frontend/src/Login.test.js
+++ frontend/src/Login.test.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Chat from "./Chat";
+import Login from "./Login";
 
 import auth from "./auth";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
-  ReactDOM.render(<Chat auth={auth} />, div);
+  ReactDOM.render(<Login auth={auth} />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
