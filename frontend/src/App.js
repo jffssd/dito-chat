aef72f4bb3c9bae66ed3f681b79dffc9a553a5fc
Arthur Bailão <arthurbailao@gmail.com>
2019-02-13 19:53:40 -0200

    extracting auth to a separated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,46 +1,14 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import Login from "./Login";
 import Chat from "./Chat";
 
-const USER_KEY = "dito-chat-user";
-
-const generateId = () =>
-  Math.random()
-    .toString(36)
-    .substring(2, 15) +
-  Math.random()
-    .toString(36)
-    .substring(2, 15);
-
-const auth = {
-  user: null,
-  authenticate(name) {
-    this.user = {
-      name,
-      id: generateId()
-    };
-    localStorage.setItem(USER_KEY, JSON.stringify(this.user));
-  },
-
-  logout() {
-    localStorage.removeItem(USER_KEY);
-    this.user = null;
-    window.location.reload();
-  },
-
-  isAuthenticated() {
-    if (this.user !== null) return true;
-
-    const cache = localStorage.getItem(USER_KEY);
-    if (cache != null) {
-      this.user = JSON.parse(cache);
-      return true;
-    }
-
-    return false;
-  }
-};
+import auth from "./auth";
 
 const PrivateRoute = ({ component: Component, ...rest }) => (
   <Route
